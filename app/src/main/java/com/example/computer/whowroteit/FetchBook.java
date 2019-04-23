c8a2d8c211094c4dcc3044649696d95d86a9508a
segunfrancis <francis4dblues@gmail.com>
2019-04-23 17:03:13 +0100

    Add NetworkUtils class

--- app/src/main/java/com/example/computer/whowroteit/FetchBook.java
+++ app/src/main/java/com/example/computer/whowroteit/FetchBook.java
@@ -1,6 +1,32 @@
 package com.example.computer.whowroteit;
 
 import android.os.AsyncTask;
+import android.widget.TextView;
 
-public class FetchBook extends AsyncTask {
+import java.lang.ref.WeakReference;
+
+public class FetchBook extends AsyncTask<String, Void, String> {
+    /*
+     * First parameter - input-parameter type
+     * Second parameter - progress-indicator type
+     * Third parameter - result type
+     */
+
+    private WeakReference<TextView> mTitleText;
+    private WeakReference<TextView> mAuthorText;
+
+    public FetchBook(TextView titleText, TextView authorText) {
+        this.mTitleText = new WeakReference<>(titleText);
+        this.mAuthorText = new WeakReference<>(authorText);
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+    }
 }
