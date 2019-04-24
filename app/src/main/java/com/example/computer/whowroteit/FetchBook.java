ba9afc337d879f7d9bab850910f74079f62f9d1a
segunfrancis <francis4dblues@gmail.com>
2019-04-24 05:23:04 +0100

    Update onPostExecute method

--- app/src/main/java/com/example/computer/whowroteit/FetchBook.java
+++ app/src/main/java/com/example/computer/whowroteit/FetchBook.java
@@ -3,6 +3,10 @@ package com.example.computer.whowroteit;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.lang.ref.WeakReference;
 
 public class FetchBook extends AsyncTask<String, Void, String> {
@@ -29,5 +33,46 @@ public class FetchBook extends AsyncTask<String, Void, String> {
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+
+        try {
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            while (i < itemsArray.length() && (authors == null && title == null)) {
+                // Get the current item information
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    // if onPostExecute does not receive a proper JSON string,
+                    // update the UI to show failed results.
+                    mTitleText.get().setText(R.string.no_results);
+                    mAuthorText.get().setText("");
+                    e.printStackTrace();
+                }
+                // Move to the next item
+                i++;
+
+                // Because the references to the TextView objects are WeakReference
+                // objects, you have to dereference them using the get() method.
+                if (title != null && authors != null) {
+                    mTitleText.get().setText(title);
+                    mAuthorText.get().setText(authors);
+                } else {
+                    mTitleText.get().setText(R.string.no_results);
+                    mAuthorText.get().setText("");
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
