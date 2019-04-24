2f57fed560e4b3422ad2e9026c1114710ef447b5
segunfrancis <francis4dblues@gmail.com>
2019-04-24 07:53:11 +0100

    Save state

--- app/src/main/java/com/example/computer/whowroteit/MainActivity.java
+++ app/src/main/java/com/example/computer/whowroteit/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.computer.whowroteit;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +14,10 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
     private EditText mBookInput;
     private TextView mTitleText;
     private TextView mAuthorText;
@@ -23,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
         mBookInput = findViewById(R.id.book_input);
         mTitleText = findViewById(R.id.title_text);
         mAuthorText = findViewById(R.id.author_text);
+
+        // Save state
+        if (getSupportLoaderManager().getLoader(0) != null) {
+            getSupportLoaderManager().initLoader(0, null, this);
+        }
     }
 
     public void searchBooks(View view) {
@@ -40,7 +52,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (networkInfo != null && networkInfo.isConnected() && queryString.length() != 0) {
-            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            //new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, this);
             mAuthorText.setText("");
             mTitleText.setText(R.string.loading);
         } else {
@@ -52,5 +67,75 @@ public class MainActivity extends AppCompatActivity {
                 mTitleText.setText(R.string.no_network);
             }
         }
+    }
+
+    /**
+     * onCreateLoader() is called when you instantiate your loader.
+     * onLoadFinished() is called when the loader's task finishes.
+     * This is where you add the code to update your UI with the results.
+     * onLoaderReset() cleans up any remaining resources.
+     */
+
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle bundle) {
+        String queryString = "";
+        if (bundle != null) {
+            queryString = bundle.getString("queryString");
+        }
+        return new BookLoader(this, queryString);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+        try {
+            JSONObject jsonObject = new JSONObject(data);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            // Look for results in the items array, exiting when both the title
+            // and author are found or when all items have been checked.
+            while (i < itemsArray.length() && (authors == null && title == null)) {
+                // Get the current item information
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                // Move to the next item
+                i++;
+
+                // Because the references to the TextView objects are WeakReference
+                // objects, you have to dereference them using the get() method.
+                if (title != null && authors != null) {
+                    mTitleText.setText(title);
+                    mAuthorText.setText(authors);
+                } else {
+                    mTitleText.setText(R.string.no_results);
+                    mAuthorText.setText("");
+                }
+            }
+        } catch (Exception e) {
+            // if onPostExecute does not receive a proper JSON string,
+            // update the UI to show failed results.
+            mTitleText.setText(R.string.no_results);
+            mAuthorText.setText("");
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
     }
 }
